98edb143451cc8f2650441e48412f27ed7a35436
Kate Kim <katehrkim@gmail.com>
2021-06-24 21:52:00 -0400

    Kate's answer

--- chicago.py
+++ chicago.py
@@ -1,1 +1,43 @@
 # Your code goes here!
+import csv
+import psycopg2
+import os
+
+import ipdb
+from decimal import Decimal
+
+def table_creation_query():
+    return "CREATE TABLE employees (first_name varchar(255) NOT NULL, last_name varchar(255) NOT NULL, job_title varchar(255) NOT NULL, full_or_part_time char(1) NOT NULL, department varchar(255) NOT NULL, annual_salary money NOT NULL);"
+
+def clean_data(csv_row):
+    cleaned = {}
+    full_name = csv_row['Name'].split(',  ')
+    cleaned['first_name'] = full_name[1]
+    cleaned['last_name'] = full_name[0]
+    cleaned['job_title'] = csv_row['Job Titles']
+    cleaned['full_or_part_time'] = csv_row['Full or Part-Time']
+    cleaned['department'] = csv_row['Department']
+    salary_or_hourly = csv_row['Salary or Hourly']
+    if salary_or_hourly == 'Salary':
+        annual_salary = Decimal(csv_row['Annual Salary'])
+    else:
+        annual_salary = int(csv_row['Typical Hours']) * Decimal(csv_row['Hourly Rate']) * 50
+    cleaned['annual_salary'] = annual_salary
+    return cleaned
+
+connection = psycopg2.connect(f"dbname=chicago_salaries user={os.getlogin()}")
+
+with connection.cursor() as cursor:
+    cursor.execute(table_creation_query())
+
+    dir_path = os.getcwd()
+    csv_path = os.path.join(dir_path, "Current_Employee_Names__Salaries__and_Position_Titles.csv")
+    with open(csv_path) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            cleaned_data = clean_data(row)
+            cursor.execute("INSERT INTO employees (first_name, last_name, job_title, full_or_part_time, department, annual_salary) VALUES (%s, %s, %s, %s, %s, %s);", (cleaned_data['first_name'], cleaned_data['last_name'], cleaned_data['job_title'], cleaned_data['full_or_part_time'], cleaned_data['department'], cleaned_data['annual_salary'],))
+
+connection.commit()
+cursor.close()
+connection.close()
--- chicago_queries.sql
+++ chicago_queries.sql
@@ -1,1 +1,68 @@
 -- Write your Queries here
+-- 1. Find the employee being paid the most
+SELECT first_name, last_name, annual_salary
+FROM employees
+ORDER BY annual_salary DESC
+LIMIT 1;
+
+-- 2. Find the employee being paid the least
+
+SELECT first_name, last_name, annual_salary
+FROM employees
+ORDER BY annual_salary ASC
+LIMIT 1;
+
+-- 3. Find the department with the highest average salary
+
+SELECT department, AVG(CAST(annual_salary AS DECIMAL))
+FROM employees
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1;
+
+-- 4. Find the department with the lowest average salary
+
+SELECT department, AVG(CAST(annual_salary AS DECIMAL))
+FROM employees
+GROUP BY 1
+ORDER BY 2 ASC
+LIMIT 1;
+
+-- 5. Find the average salary difference between full time and part time workers
+
+SELECT AVG(CAST(annual_salary AS DECIMAL)) - 
+(SELECT AVG(CAST(annual_salary AS DECIMAL))
+FROM employees
+WHERE full_or_part_time = 'P')
+AS salary_difference
+FROM employees
+WHERE full_or_part_time = 'F';
+
+-- 6. Find the most common first name
+
+SELECT first_name, COUNT(first_name) AS occurrences
+FROM employees
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1;
+
+-- 7. Find the most common last name
+
+SELECT last_name, COUNT(first_name) AS occurrences
+FROM employees
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1;
+
+-- 8. If there are people with the same name, find what their job titles, departments, and annual salaries are
+
+SELECT *
+FROM employees emp1
+WHERE exists(
+  SELECT *
+  FROM employees emp2
+  WHERE emp1.first_name = emp2.first_name
+  AND emp1.last_name = emp2.last_name
+  AND emp1.annual_salary <> emp2.annual_salary
+)
+ORDER BY first_name, last_name;
